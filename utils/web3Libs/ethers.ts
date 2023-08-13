25641136ea3f75543debcb75d19f597896b7ecf6
sunnyRK <radadiyasunny970@gmail.com>
2023-08-13 20:59:47 +0530

    added erc20 methods

--- utils/web3Libs/ethers.ts
+++ utils/web3Libs/ethers.ts
@@ -1,5 +1,6 @@
 import { BigNumber, ethers } from "ethers"
 import { rpscURLS } from "../constants"
+import IERC20 from "../../abis/IERC20.json";
 
 export const getProvider = async (toChainId) => {
     try {
@@ -21,11 +22,43 @@ export const getContractInstance = async (address, abi, provider) : Promise<ethe
     }
 }
 
+export async function getErc20Data(token, address, spender, provider) {
+    try {
+    console.log('getErc20Data', token, address, spender);
+      if (!address || !spender) throw 'Invalid addresses';
+      console.log('getErc20Data', token, address, spender);
+      const erc20: any = await getContractInstance(token, IERC20, provider);
+      console.log('getErc20Data', erc20);
+
+      const [name, symbol, decimals, totalSupply, balance, allowance] =
+        await Promise.all([
+          erc20.name(),
+          erc20.symbol(),
+          erc20.decimals(),
+          erc20.totalSupply(),
+          erc20.balanceOf(address),
+          erc20.allowance(address, spender),
+        ]);
+      return { name, symbol, decimals, totalSupply, balance, allowance };
+    } catch (error) {
+      console.log('ERC20Data-error', error);
+    }
+  }
+
 export const getErc20Balanceof = async (erc20, address): Promise<BigNumber | undefined> => {
     try {
         return await erc20.balanceOf(address)
     } catch (error) {
         console.log("getErc20Balanceof-error", error)
+        return
+    }
+}
+
+export const getErc20Decimals = async (erc20): Promise<BigNumber | undefined> => {
+    try {
+        return await erc20.decimals()
+    } catch (error) {
+        console.log("getErc20Decimals-error", error)
         return
     }
 }
