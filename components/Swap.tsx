d329fe9a570926d3f38d6a5a94a9c77a36dc6aae
sunnyRK <radadiyasunny970@gmail.com>
2023-08-26 16:47:50 +0530

    prettier changes

--- components/Swap.tsx
+++ components/Swap.tsx
@@ -1,28 +1,27 @@
-import * as React from "react"
-import {BigNumber as bg} from "bignumber.js"
-import {useAppStore, useSwapAppStore} from "../store/appStore"
-import {V3_SWAP_ROUTER_ADDRESS, _functionType, _nonce} from "../utils/constants"
-import {useUniswap} from "../hooks/useUniswap"
-import {BigNumber, ethers} from "ethers"
-import {useApprove} from "../hooks/useApprove"
-import {useEoaProvider} from "../hooks/aaProvider/useEoaProvider"
-import {useBiconomyProvider} from "../hooks/aaProvider/useBiconomyProvider"
-import {getErc20Decimals, getProvider} from "../utils/web3Libs/ethers"
-import IERC20 from "../abis/IERC20.json"
-import {useAddress} from "@thirdweb-dev/react"
+import * as React from "react";
+import { BigNumber as bg } from "bignumber.js";
+import { useAppStore, useSwapAppStore } from "../store/appStore";
+import { V3_SWAP_ROUTER_ADDRESS, _functionType, _nonce } from "../utils/constants";
+import { useUniswap } from "../hooks/useUniswap";
+import { BigNumber, ethers } from "ethers";
+import { useApprove } from "../hooks/useApprove";
+import { useEoaProvider } from "../hooks/aaProvider/useEoaProvider";
+import { useBiconomyProvider } from "../hooks/aaProvider/useBiconomyProvider";
+import { getErc20Decimals, getProvider } from "../utils/web3Libs/ethers";
+import IERC20 from "../abis/IERC20.json";
+import { useAddress } from "@thirdweb-dev/react";
 
 export default function Swap() {
-    const address = useAddress() // Detect the connected address
-    const {mutateAsync: swap} = useUniswap()
-    const {mutateAsync: approve} = useApprove()
-    const {mutateAsync: sendTxTrditionally} = useEoaProvider()
-    const {mutateAsync: sendToBiconomy} = useBiconomyProvider()
+    const address = useAddress(); // Detect the connected address
+    const { mutateAsync: swap } = useUniswap();
+    const { mutateAsync: approve } = useApprove();
+    const { mutateAsync: sendTxTrditionally } = useEoaProvider();
+    const { mutateAsync: sendToBiconomy } = useBiconomyProvider();
 
-    const {smartAccount}: any = useAppStore((state) => state)
-    const [tokenInDecimals, setTokenInDecimals] = React.useState<any>(6)
-    const [tokenOutDecimals, setTokenOutDecimals] = React.useState<any>(6)
-    const [amountOutAfterSlippage, setAmountOutAfterSlippage] =
-        React.useState<any>()
+    const { smartAccount }: any = useAppStore((state) => state);
+    const [tokenInDecimals, setTokenInDecimals] = React.useState<any>(6);
+    const [tokenOutDecimals, setTokenOutDecimals] = React.useState<any>(6);
+    const [amountOutAfterSlippage, setAmountOutAfterSlippage] = React.useState<any>();
 
     const {
         setTokenIn,
@@ -35,150 +34,122 @@ export default function Swap() {
         amountOut,
         setSlippage,
         slippage,
-    }: any = useSwapAppStore((state) => state)
+    }: any = useSwapAppStore((state) => state);
 
     const handleTokenIn = async (_tokenIn: any) => {
-        setTokenIn(_tokenIn)
-        const web3JsonProvider = await getProvider("109")
-        const erc20 = await new ethers.Contract(
-            _tokenIn,
-            IERC20,
-            web3JsonProvider
-        )
-        const decimals = await getErc20Decimals(erc20)
-        setTokenInDecimals(decimals)
-    }
+        setTokenIn(_tokenIn);
+        const web3JsonProvider = await getProvider("109");
+        const erc20 = await new ethers.Contract(_tokenIn, IERC20, web3JsonProvider);
+        const decimals = await getErc20Decimals(erc20);
+        setTokenInDecimals(decimals);
+    };
     const handleTokenOut = async (_tokenOut: any) => {
-        setTokenOut(_tokenOut)
-        const web3JsonProvider = await getProvider("109")
-        const erc20 = await new ethers.Contract(
-            _tokenOut,
-            IERC20,
-            web3JsonProvider
-        )
-        const decimals = await getErc20Decimals(erc20)
-        setTokenOutDecimals(decimals)
-    }
+        setTokenOut(_tokenOut);
+        const web3JsonProvider = await getProvider("109");
+        const erc20 = await new ethers.Contract(_tokenOut, IERC20, web3JsonProvider);
+        const decimals = await getErc20Decimals(erc20);
+        setTokenOutDecimals(decimals);
+    };
     const handleAmountIn = async (_amountIn) => {
         if (!smartAccount) {
-            alert("You need to biconomy login")
-            return
+            alert("You need to biconomy login");
+            return;
         }
         if (_amountIn) {
-            let amountInByDecimals = bg(_amountIn)
-            amountInByDecimals = amountInByDecimals.multipliedBy(
-                bg(10).pow(tokenInDecimals)
-            )
+            let amountInByDecimals = bg(_amountIn);
+            amountInByDecimals = amountInByDecimals.multipliedBy(bg(10).pow(tokenInDecimals));
             if (amountInByDecimals.eq(0)) {
-                setAmountIn(_amountIn)
+                setAmountIn(_amountIn);
             } else {
-                setAmountIn(amountInByDecimals.toString())
+                setAmountIn(amountInByDecimals.toString());
             }
         } else {
-            console.log("5")
-            setAmountIn("")
+            console.log("5");
+            setAmountIn("");
         }
-        console.log("6")
-    }
+        console.log("6");
+    };
 
     const handleAmountOut = async (_amountOut: any) => {
-        setAmountOut(_amountOut)
-    }
+        setAmountOut(_amountOut);
+    };
     const handleSlippage = async (_slippage: any) => {
-        console.log("_slippage", _slippage)
-        setSlippage(_slippage)
-        console.log("amountOut", amountOut.toString())
-        const amountAfterSlippage = bg(amountOut).minus(
-            bg(amountOut).multipliedBy(_slippage).div(100)
-        )
-        console.log("amountAfterSlippage", amountAfterSlippage.toString())
-        setAmountOutAfterSlippage(amountAfterSlippage)
-    }
+        console.log("_slippage", _slippage);
+        setSlippage(_slippage);
+        console.log("amountOut", amountOut.toString());
+        const amountAfterSlippage = bg(amountOut).minus(bg(amountOut).multipliedBy(_slippage).div(100));
+        console.log("amountAfterSlippage", amountAfterSlippage.toString());
+        setAmountOutAfterSlippage(amountAfterSlippage);
+    };
 
     const sendTx = async (isScw: boolean) => {
         try {
-            if (!smartAccount) throw "please login by scw"
-            if (!address) throw "please login by metamask"
-            const web3JsonProvider = await getProvider("109")
-            let _address = isScw ? smartAccount.address : address
-            let _provider = isScw ? smartAccount.provider : web3JsonProvider
-            alert("_address: " + _address)
+            if (!smartAccount) throw "please login by scw";
+            if (!address) throw "please login by metamask";
+            const web3JsonProvider = await getProvider("109");
+            let _address = isScw ? smartAccount.address : address;
+            let _provider = isScw ? smartAccount.provider : web3JsonProvider;
+            alert("_address: " + _address);
             const approveData = await approve({
                 tokenIn,
                 spender: V3_SWAP_ROUTER_ADDRESS,
                 amountIn,
                 address: _address,
                 web3JsonProvider: _provider,
-            })
-            console.log("approveData--", approveData, amountIn.toString())
+            });
+            console.log("approveData--", approveData, amountIn.toString());
             const swapData = await swap({
                 tokenIn, //: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
                 tokenOut, //: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
                 amountIn, //: BigNumber.from('1000000'),
                 address: _address,
                 type: "exactIn",
-            })
-            console.log("swapData--", swapData)
+            });
+            console.log("swapData--", swapData);
 
             if (swapData?.amountOutprice) {
-                let amountOutByDecimals = bg(swapData?.amountOutprice)
-                amountOutByDecimals = amountOutByDecimals.multipliedBy(
-                    bg(10).pow(tokenOutDecimals)
-                )
+                let amountOutByDecimals = bg(swapData?.amountOutprice);
+                amountOutByDecimals = amountOutByDecimals.multipliedBy(bg(10).pow(tokenOutDecimals));
                 if (amountOutByDecimals.eq(0)) {
-                    setAmountOut(swapData?.amountOutprice)
+                    setAmountOut(swapData?.amountOutprice);
                     if (BigNumber.from(slippage).gt(0)) {
-                        const amountAfterSlippage = bg(
-                            swapData?.amountOutprice
-                        ).minus(
-                            bg(swapData?.amountOutprice)
-                                .multipliedBy(slippage)
-                                .div(100)
-                        )
-                        console.log(
-                            "amountAfterSlippage-1",
-                            amountAfterSlippage.toString()
-                        )
-                        setAmountOutAfterSlippage(amountAfterSlippage)
+                        const amountAfterSlippage = bg(swapData?.amountOutprice).minus(
+                            bg(swapData?.amountOutprice).multipliedBy(slippage).div(100)
+                        );
+                        console.log("amountAfterSlippage-1", amountAfterSlippage.toString());
+                        setAmountOutAfterSlippage(amountAfterSlippage);
                     }
                 } else {
-                    setAmountOut(amountOutByDecimals.toString())
+                    setAmountOut(amountOutByDecimals.toString());
                     if (BigNumber.from(slippage).gt(0)) {
-                        const amountAfterSlippage = bg(
-                            amountOutByDecimals
-                        ).minus(
-                            bg(amountOutByDecimals)
-                                .multipliedBy(slippage)
-                                .div(100)
-                        )
-                        console.log(
-                            "amountAfterSlippage-2",
-                            amountAfterSlippage.toString()
-                        )
-                        setAmountOutAfterSlippage(amountAfterSlippage)
+                        const amountAfterSlippage = bg(amountOutByDecimals).minus(
+                            bg(amountOutByDecimals).multipliedBy(slippage).div(100)
+                        );
+                        console.log("amountAfterSlippage-2", amountAfterSlippage.toString());
+                        setAmountOutAfterSlippage(amountAfterSlippage);
                     }
                 }
             } else {
-                setAmountOut("")
+                setAmountOut("");
             }
 
             if (isScw) {
                 if (approveData) {
-                    await sendToBiconomy([approveData, swapData?.swapTx])
+                    await sendToBiconomy([approveData, swapData?.swapTx]);
                 } else {
-                    await sendToBiconomy([swapData?.swapTx])
+                    await sendToBiconomy([swapData?.swapTx]);
                 }
             } else {
                 if (approveData) {
-                    await sendTxTrditionally([approveData, swapData?.swapTx])
+                    await sendTxTrditionally([approveData, swapData?.swapTx]);
                 } else {
-                    await sendTxTrditionally([swapData?.swapTx])
+                    await sendTxTrditionally([swapData?.swapTx]);
                 }
             }
         } catch (error) {
-            console.log("checkStackUp-error", error)
+            console.log("checkStackUp-error", error);
         }
-    }
+    };
 
     return (
         <>
@@ -199,7 +170,7 @@ export default function Swap() {
 
             {smartAccount && (
                 <div>
-                    <div style={{margin: "5px"}}>
+                    <div style={{ margin: "5px" }}>
                         <h1>token0</h1>
                         <input
                             placeholder="token0"
@@ -209,55 +180,41 @@ export default function Swap() {
                         />
                     </div>
 
-                    <div style={{margin: "5px"}}>
+                    <div style={{ margin: "5px" }}>
                         <h1>token1</h1>
                         <input
                             placeholder="token1"
                             type="text"
                             value={tokenOut}
-                            onChange={(e: any) =>
-                                handleTokenOut(e.target.value)
-                            }
+                            onChange={(e: any) => handleTokenOut(e.target.value)}
                         />
                     </div>
 
-                    <div style={{margin: "5px"}}>
+                    <div style={{ margin: "5px" }}>
                         <h1>amountIn</h1>
                         <input
                             placeholder="amountIn"
                             type="number"
                             value={
                                 amountIn != 0
-                                    ? bg(amountIn)
-                                          .dividedBy(
-                                              bg(10).pow(tokenInDecimals)
-                                          )
-                                          .toString()
+                                    ? bg(amountIn).dividedBy(bg(10).pow(tokenInDecimals)).toString()
                                     : amountIn
                             }
-                            onChange={(e: any) =>
-                                handleAmountIn(e.target.value)
-                            }
+                            onChange={(e: any) => handleAmountIn(e.target.value)}
                         />
                     </div>
 
-                    <div style={{margin: "5px"}}>
+                    <div style={{ margin: "5px" }}>
                         <h1>amountOut</h1>
                         <input
                             placeholder="amountOut"
                             type="number"
                             value={
                                 amountOutAfterSlippage != 0
-                                    ? bg(amountOutAfterSlippage)
-                                          .dividedBy(
-                                              bg(10).pow(tokenOutDecimals)
-                                          )
-                                          .toString()
+                                    ? bg(amountOutAfterSlippage).dividedBy(bg(10).pow(tokenOutDecimals)).toString()
                                     : amountOutAfterSlippage
                             }
-                            onChange={(e: any) =>
-                                handleAmountOut(e.target.value)
-                            }
+                            onChange={(e: any) => handleAmountOut(e.target.value)}
                         />
                     </div>
 
@@ -271,19 +228,15 @@ export default function Swap() {
             />
           </div> */}
 
-                    <div style={{margin: "5px"}}>
-                        <button onClick={() => sendTx(true)}>
-                            SendTx By SCW
-                        </button>
+                    <div style={{ margin: "5px" }}>
+                        <button onClick={() => sendTx(true)}>SendTx By SCW</button>
                     </div>
 
-                    <div style={{margin: "5px"}}>
-                        <button onClick={() => sendTx(false)}>
-                            SendTx BT EOA
-                        </button>
+                    <div style={{ margin: "5px" }}>
+                        <button onClick={() => sendTx(false)}>SendTx BT EOA</button>
                     </div>
                 </div>
             )}
         </>
-    )
+    );
 }
