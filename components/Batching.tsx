7118ab3e0bd24a1b694c7b81b3971c299bc24879
parilbeladiya <parilbeladiya678@gmail.com>
2023-09-03 22:07:30 +0530

    feat: add batching for landing + cross chain difi ui

--- components/Batching.tsx
+++ components/Batching.tsx
@@ -38,7 +38,14 @@ import { useCalculatebalance } from "../hooks/useCalculateBalance";
 import { getContractInstance, getErc20Balanceof, getErc20Decimals, getProvider } from "../utils/web3Libs/ethers";
 import IERC20 from "../abis/IERC20.json";
 import { BigNumber, ethers } from "ethers";
-import { FiCopy } from "react-icons/fi";
+import { LiaChevronDownSolid, LiaChevronUpSolid } from "react-icons/lia";
+import { RiAddCircleLine, RiDeleteBin5Fill } from "react-icons/ri";
+import { BiSolidRightArrowCircle } from "react-icons/bi";
+
+import { MdDelete } from "react-icons/md";
+
+import { TbSquareRoundedChevronDownFilled, TbSquareRoundedChevronUpFilled } from "react-icons/tb";
+
 import { useRefinance } from "../hooks/Batching/useRefinance";
 import {
     abiFetcher,
@@ -52,6 +59,7 @@ import aave_v2_Abi from "../abis/defi/aave_v2.json";
 import IndividualBatch from "./BatchingComponenets/IndividualBatch";
 import { useBiconomyProvider } from "../hooks/aaProvider/useBiconomyProvider";
 import { useEoaProvider } from "../hooks/aaProvider/useEoaProvider";
+import { format } from "date-fns";
 bg.config({ DECIMAL_PLACES: 10 });
 
 export default function Batching() {
@@ -88,7 +96,11 @@ export default function Batching() {
     // const [individualBatch, setIndividualBatch] = React.useState<number[]>([]);
     // const [collectedValues, setCollectedValues] = React.useState<string[][]>([]);
     // const [currentRow, setCurrentRow] = React.useState<string[]>([]);
-    const [individualBatch, setIndividualBatch] = React.useState<{ id: number; txHash: string[] }[]>([]);
+    const [individualBatch, setIndividualBatch] = React.useState<{ id: number; txHash: string[] }[]>([
+        { id: 1, txHash: [""] },
+    ]);
+    console.log("🚀 ~ file: Batching.tsx:95 ~ Batching ~ individualBatch:", individualBatch);
+    const [showIndividualBatchList, setShowIndividualBatchList] = React.useState<any>(null);
     const [allTxs, setCollectedValues] = React.useState<any>([]);
 
     // useEffect(() => {
@@ -172,6 +184,7 @@ export default function Batching() {
 
     const updateInputValues = (id: number, txHash: string[]) => {
         setIndividualBatch((prevInputBars) => prevInputBars.map((bar) => (bar.id === id ? { ...bar, txHash } : bar)));
+        addBatch();
     };
 
     // const collectInputValues = async () => {
@@ -382,61 +395,144 @@ export default function Batching() {
             console.log("sendBatch-error", error);
         }
     };
+
+    const toggleShowBatchList = (id: number): void => {
+        if (showIndividualBatchList === id) {
+            setShowIndividualBatchList(null);
+        } else {
+            setShowIndividualBatchList(id);
+        }
+    };
+
     return (
         <>
-            <div className="main-container flex justify-start items-start gap-3">
-                {true && (
-                    <div className="w-full h-[calc(1000vh-108px)] bg-gradient-to-r from-primary-950 via-primary-600 to-primary-950 flex flex-col gap-5 border-2 border-secondary-800 shadow-sm shadow-primary-950 rounded-lg cursor-pointer p-10">
-                        {individualBatch.map((bar) => (
+            <div className="flex flex-col gap-3">
+                <div className="flex justify-between items-center gap-3 py-1">
+                    <button
+                        type="button"
+                        onClick={addBatch}
+                        className="flex justify-center items-center gap-2 bg-slate-900 hover:bg-slate-950 py-2 px-5 rounded-lg text-white font-medium border-b-4 border-slate-600 hover:border-slate-700 transition duration-300"
+                    >
+                        <RiAddCircleLine size="20px" /> Add New Batch Bar
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => sendBatchAll(true)}
+                        className="flex justify-center items-center gap-2 bg-success-600 hover:bg-success-700 py-2 px-5 rounded-lg text-white font-medium border-b-4 border-success-800 hover:border-success-900 transition duration-300"
+                    >
+                        {sendTxLoading && <ImSpinner className="animate-spin h-5 w-5" />}
+                        Excecute Batch {!sendTxLoading && <BiSolidRightArrowCircle size="20px" />}
+                    </button>
+                    {/* <button
+                        type="button"
+                        onClick={() => sendBatchAll(false)}
+                        className="flex justify-center items-center gap-2 bg-success-600 hover:bg-success-700 py-2 px-5 rounded-lg text-white font-medium border-b-4 border-success-800 hover:border-success-900 transition duration-300"
+                    >
+                        {sendTxLoadingForEoa && <ImSpinner className="animate-spin h-5 w-5" />}
+                        Send Batches via EOA
+                    </button> */}
+                </div>
+                <div className="flex flex-col sm:flex-row justify-center items-start gap-4">
+                    {true && (
+                        <div className="w-full min-h-[calc(100vh-225px)] bg-gradient-to-t from-gray-200 via-white to-gray-200 flex flex-col gap-5 shadow-md shadow-primary-950 rounded-lg cursor-pointer p-10">
+                            <h1 className="text-lg md:text-xl lg:text-2xl text-center font-extrabold mb-5">
+                                Cross-Chain Lending and Batching
+                            </h1>
+
+                            {/* {individualBatch.map((bar) => (
+                                <IndividualBatch
+                                    key={bar.id}
+                                    // values={bar.txHash}
+                                    onUpdate={(newValues) => updateInputValues(bar.id, newValues)}
+                                />
+                            ))} */}
+
                             <IndividualBatch
-                                key={bar.id}
+                                key={individualBatch[individualBatch.length - 1]?.id}
                                 // values={bar.txHash}
-                                onUpdate={(newValues) => updateInputValues(bar.id, newValues)}
+                                onUpdate={(newValues) =>
+                                    updateInputValues(individualBatch[individualBatch.length - 1]?.id, newValues)
+                                }
                             />
-                        ))}
-
-                        <div className="flex justify-center items-center gap-3 py-3">
-                            <button
-                                type="button"
-                                onClick={addBatch}
-                                className="flex justify-center items-center gap-2 bg-slate-900 hover:bg-slate-950 py-2 px-5 rounded-lg text-white font-medium border-b-4 border-success-800 hover:border-success-900 transition duration-300"
-                            >
-                                + Add New Batch Bar
-                            </button>
                         </div>
+                    )}
+                    <div className="w-full min-h-[calc(100vh-225px)] bg-gradient-to-t from-gray-200 via-white to-gray-200 text-center flex flex-col gap-3 shadow-md shadow-primary-950 rounded-lg cursor-pointer p-10">
+                        {individualBatch.length > 0 ? (
+                            individualBatch.map((bar) => (
+                                <div key={bar.id} className="relative">
+                                    <div className="simulation-success flex justify-between items-center gap-5 bg-black py-2 px-3 rounded-lg text-primary-100 font-medium   transition duration-300">
+                                        <div className="flex justify-start items-baseline gap-2">
+                                            <h1 className="flex justify-center items-center gap-3 text-white font-semibold text-base">
+                                                {bar.id}.
+                                            </h1>
+                                            <div className="flex justify-center items-center gap-2 text-white text-sm">
+                                                <span>0x{bar.id}</span>
+                                                <span>0x{bar.id}</span>
+                                            </div>
+                                        </div>
+                                        <div className="flex justify-end items-center gap-2">
+                                            <div>
+                                                <MdDelete color="red" size="20px" onClick={() => removeBatch(bar.id)} />
+                                            </div>
+                                            <span className="flex justify-center items-center gap-2">
+                                                <div onClick={() => toggleShowBatchList(bar.id)}>
+                                                    {showIndividualBatchList === bar.id ? (
+                                                        <LiaChevronUpSolid size="20px" />
+                                                    ) : (
+                                                        <LiaChevronDownSolid size="20px" />
+                                                    )}
+                                                </div>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    {showIndividualBatchList === bar.id && (
+                                        <div className="w-full my-1 z-50 flex flex-col justify-center items-start text-start gap-1 bg-gray-700 p-3 rounded-lg">
+                                            <div className="w-full text-base font-bold text-gray-400">
+                                                Interact With :-
+                                            </div>
+                                            <div className="w-full flex justify-start items-baseline gap-1 text-base font-medium text-white">
+                                                <span>matic :</span>
+                                                <span className="font-normal text-sm">0xabcdefghigk999</span>
+                                            </div>
 
-                        <div className="flex justify-center items-center gap-3 py-3">
-                            <button
-                                type="button"
-                                onClick={() => sendBatchAll(true)}
-                                className="flex justify-center items-center gap-2 bg-success-600 hover:bg-success-700 py-2 px-5 rounded-lg text-white font-medium border-b-4 border-success-800 hover:border-success-900 transition duration-300"
-                            >
-                                {sendTxLoading && <ImSpinner className="animate-spin h-5 w-5" />}
-                                Send Batches via SCW
-                            </button>
-                            <button
-                                type="button"
-                                onClick={() => sendBatchAll(false)}
-                                className="flex justify-center items-center gap-2 bg-success-600 hover:bg-success-700 py-2 px-5 rounded-lg text-white font-medium border-b-4 border-success-800 hover:border-success-900 transition duration-300"
-                            >
-                                {sendTxLoadingForEoa && <ImSpinner className="animate-spin h-5 w-5" />}
-                                Send Batches via EOA
-                            </button>
-                        </div>
+                                            <div className="w-full text-md font-semibold text-gray-400 mt-3">
+                                                Deposited Eth :-
+                                            </div>
+                                            <div className="w-full flex justify-start items-baseline gap-2 text-white">
+                                                <div className="w-60 font-medium text-sm">(address) :</div>
+                                                <div className="w-full font-normal text-xs">0x{bar.id}</div>
+                                            </div>
+                                            <div className="w-full flex justify-start items-baseline gap-2 text-white">
+                                                <div className="w-60 font-medium text-sm">onBehalfOf (address) :</div>
+                                                <div className="w-full font-normal text-xs">0x{bar.id}</div>
+                                            </div>
+                                            <div className="w-full flex justify-start items-baseline gap-2 text-white">
+                                                <div className="w-60 font-medium text-sm">refferalCode (uin16) :</div>
+                                                <div className="w-full font-normal text-xs">0</div>
+                                            </div>
+                                            <div className="w-full flex justify-start items-baseline gap-2 text-white">
+                                                <div className="w-60 font-medium text-sm">Created :</div>
+                                                <div className="w-full font-normal text-xs">
+                                                    {format(new Date(), "dd/MM/yyyy HH:mm:ss")}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            ))
+                        ) : (
+                            <div className="text-black font-semibold text-base md:text-lg">No Batches Found !</div>
+                        )}
+                    </div>
+                </div>
 
-                        <div className="flex justify-center items-center gap-3 py-5">
-                            {txhash && (
-                                <p>
-                                    <a
-                                        target="_blank"
-                                        href={`https://polygonscan.com/tx/${txhash}`}
-                                        style={{ color: "white" }}
-                                    >
-                                        TxHash : {shorten(txhash)}
-                                    </a>
-                                </p>
-                            )}
-                        </div>
+                {txhash && (
+                    <div className="flex justify-center items-center gap-3 py-5">
+                        <p>
+                            <a target="_blank" href={`https://polygonscan.com/tx/${txhash}`} style={{ color: "white" }}>
+                                TxHash : {shorten(txhash)}
+                            </a>
+                        </p>
                     </div>
                 )}
             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,17 @@
-import "../styles/globals.css"
-import 'react-tabs/style/react-tabs.css';
-import * as React from 'react';
-import type {AppProps} from "next/app"
+import "../styles/globals.css";
+import * as React from "react";
+import type { AppProps } from "next/app";
 // import $ from 'jquery';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
-import {ThirdwebProvider, metamaskWallet, useChain} from "@thirdweb-dev/react"
-import "@biconomy/web3-auth/dist/src/style.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThirdwebProvider, metamaskWallet, useChain } from "@thirdweb-dev/react";
+import "@biconomy/web3-auth/dist/src/style.css";
 import { Ethereum, Polygon, Avalanche, Arbitrum, Optimism, Base } from "@thirdweb-dev/chains";
 
-import {Inter} from "next/font/google"
-const inter = Inter({subsets: ["latin"]})
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 import ChainContext from "../Context/ChainContext";
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     const metamaskConfig = metamaskWallet({});
     const [selectedChain, setSelectedChain] = React.useState("");
 
@@ -23,21 +22,23 @@ export default function App({Component, pageProps}: AppProps) {
                 cacheTime: 1000 * 60 * 60 * 24, // 24 hours
             },
         },
-    })
+    });
 
     return (
         <>
-        {/* @ts-ignore */}
-        <ChainContext.Provider value={{ selectedChain, setSelectedChain }}>
-            <ThirdwebProvider
-                supportedWallets={[metamaskConfig]}
-                supportedChains={[Polygon, Arbitrum, Avalanche, Ethereum, Base, Optimism]}
-                clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID} activeChain={selectedChain}>
-                <QueryClientProvider client={queryClient}>
-                    <Component {...pageProps} />
-                </QueryClientProvider>
-            </ThirdwebProvider>
-        </ChainContext.Provider>
+            {/* @ts-ignore */}
+            <ChainContext.Provider value={{ selectedChain, setSelectedChain }}>
+                <ThirdwebProvider
+                    supportedWallets={[metamaskConfig]}
+                    supportedChains={[Polygon, Arbitrum, Avalanche, Ethereum, Base, Optimism]}
+                    clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+                    activeChain={selectedChain}
+                >
+                    <QueryClientProvider client={queryClient}>
+                        <Component {...pageProps} />
+                    </QueryClientProvider>
+                </ThirdwebProvider>
+            </ChainContext.Provider>
         </>
-    )
+    );
 }
