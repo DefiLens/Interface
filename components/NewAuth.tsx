e57e3eb9026fcffbc7c6b9eb0a547f22197289cc
sunnyRK <radadiyasunny970@gmail.com>
2023-09-04 15:23:45 +0530

    change imports and bug fixes

--- components/NewAuth.tsx
+++ components/NewAuth.tsx
@@ -1,33 +1,22 @@
 import { useContext } from "react";
 import { useState, useRef, useEffect } from "react";
 
-import { ethers } from "ethers";
 import { toast } from "react-hot-toast";
 import { BigNumber as bg } from "bignumber.js";
 
 import { FiCopy } from "react-icons/fi";
 import { ImSpinner } from "react-icons/im";
-import SocialLogin from "@biconomy/web3-auth";
-import { ChainId } from "@biconomy/core-types";
 import { FaChevronDown } from "react-icons/fa";
 import { IBundler, Bundler } from "@biconomy/bundler";
 import { TbSquareRoundedChevronDownFilled } from "react-icons/tb";
 import { IPaymaster, BiconomyPaymaster } from "@biconomy/paymaster";
 import { DEFAULT_ENTRYPOINT_ADDRESS, BiconomySmartAccountConfig, BiconomySmartAccount } from "@biconomy/account";
-import {
-    useSwitchChain,
-    useSigner,
-    useNetworkMismatch,
-    useConnect,
-    useChain,
-    useAddress,
-    metamaskWallet,
-} from "@thirdweb-dev/react";
+import { useSwitchChain, useSigner, useConnect, useChain, useAddress, metamaskWallet } from "@thirdweb-dev/react";
 
 import { useAppStore } from "../store/appStore";
 import ChainContext from "../Context/ChainContext";
 import { useCalculatebalance } from "../hooks/useCalculateBalance";
-import { rpscURLS, paymasterURLs, gasFeesNames, buttonStyle, bundlerURLs } from "../utils/constants";
+import { paymasterURLs, gasFeesNames, buttonStyle, bundlerURLs } from "../utils/constants";
 
 bg.config({ DECIMAL_PLACES: 5 });
 
@@ -47,32 +36,26 @@ export default function Home() {
     }: any = useAppStore((state) => state);
     const { selectedChain, setSelectedChain, selectedChainId, setSelectedChainId } = useContext(ChainContext);
     const { mutateAsync: fetchNativeBalance } = useCalculatebalance();
-    const [interval, enableInterval] = useState(false);
-    const sdkRef = useRef<SocialLogin | null>(null);
-    const isOnWrongNetwork = useNetworkMismatch(); // Detect if the user is on the wrong network
     const switchChain = useSwitchChain();
     const metamaskConfig = metamaskWallet();
     const connect = useConnect();
     const address: any = useAddress(); // Detect the connected address
     const signer: any = useSigner(); // Detect the connected address
     const chain = useChain();
+    const [interval, enableInterval] = useState(false);
     const [isWrongNetwork, setisWrongNetwork] = useState(false);
-    const chainIds = [137, 42161, 43114, 1, 10, 8453];
 
     useEffect(() => {
-        // alert("Hello")
         async function changeWallet() {
             if (address && smartAccount && chain) {
                 if (smartAccount.owner == address) return;
-                console.log("address1", selectedChainId, selectedChain, chain);
+                // console.log("ChainDetails: ", selectedChainId, selectedChain, chain);
                 const _smartAccount = await login(chain?.chainId);
                 console.log("_smartAccount_", _smartAccount);
                 // @ts-ignore
                 await isNetworkCorrect(chain?.chainId, _smartAccount.address);
                 setSelectedChain?.(chain.slug);
                 setSelectedChainId?.(chain?.chainId.toString());
-            } else {
-                console.log("address2");
             }
         }
         changeWallet();
@@ -105,18 +88,14 @@ export default function Home() {
     const isNetworkCorrect = async (chainId: any, smartAccountAddress) => {
         try {
             const chainIds = [137, 42161, 10, 1, 43114, 8453];
-            console.log("1");
             if (chainIds.includes(chainId)) {
-                console.log("2", smartAccount?.address);
                 setisWrongNetwork(false);
                 await fetchNativeBalance({ chainId: chainId, eoaAddress: address, scwAddress: smartAccountAddress });
-                console.log("2.1");
             } else {
-                console.log("3");
                 setisWrongNetwork(true);
             }
         } catch (error) {
-            console.log("isNetworkCorrect-error: ", error);
+            console.log("isNetworkCorrect:error: ", error);
         }
     };
 
@@ -140,7 +119,6 @@ export default function Home() {
         //     sdkRef.current.showWallet();
         //     enableInterval(true);
         // } else {
-        console.log("Hello");
         return setupSmartAccount(chainId);
         // }
     }
@@ -148,13 +126,10 @@ export default function Home() {
     async function setupSmartAccount(chainId) {
         // if (!sdkRef?.current?.provider) return;
         // sdkRef.current.hideWallet();
-        console.log("Hello2");
-
         setLoading(true);
         // const web3Provider = new ethers.providers.Web3Provider(sdkRef.current.provider);
         try {
             const smartAccount = await createAccount(chainId);
-            console.log("smartAccount", smartAccount);
             setSmartAccount(smartAccount);
             setLoading(false);
             setCurrentProvider("Biconomy");
@@ -253,9 +228,6 @@ export default function Home() {
                 setConnected(true);
             })
             .catch(setConnected(false));
-        // if (chain?.chainId) {
-        //   await login(chain?.chainId);
-        // }
     };
 
     return (
@@ -288,10 +260,6 @@ export default function Home() {
                         </div>
                         <div className="text-white">DefiLens</div>
                     </div>
-                    {/* <a href="#" className="font-bold text-2xl hover:bg-transparent">
-                        DefiLens(Formerly ChainPing)
-                    </a> */}
-                    {/* <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall mui-style-8tazii" focusable="false" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.71542 1.06718C7.89457 0.977606 8.10543 0.977606 8.28458 1.06718L14.6481 4.249C14.8637 4.3568 14.9999 4.57714 14.9999 4.81818C14.9999 5.05922 14.8637 5.27957 14.6481 5.38736L8.28458 8.56918C8.10543 8.65876 7.89457 8.65876 7.71542 8.56918L1.35191 5.38736C1.13632 5.27957 1.00014 5.05922 1.00014 4.81818C1.00014 4.57714 1.13632 4.3568 1.35191 4.249L7.71542 1.06718ZM3.05941 4.81818L8 7.28852L12.9406 4.81818L8 2.34784L3.05941 4.81818ZM1.06732 7.71541C1.22449 7.40106 1.60673 7.27364 1.92108 7.43082L8 10.4703L14.0789 7.43082C14.3933 7.27364 14.7755 7.40106 14.9327 7.71541C15.0899 8.02976 14.9624 8.41201 14.6481 8.56918L8.28458 11.751C8.10543 11.8406 7.89457 11.8406 7.71542 11.751L1.35191 8.56918C1.03756 8.41201 0.910149 8.02976 1.06732 7.71541ZM1.06732 10.8972C1.22449 10.5829 1.60673 10.4555 1.92108 10.6126L8 13.6522L14.0789 10.6126C14.3933 10.4555 14.7755 10.5829 14.9327 10.8972C15.0899 11.2116 14.9624 11.5938 14.6481 11.751L8.28458 14.9328C8.10543 15.0224 7.89457 15.0224 7.71542 14.9328L1.35191 11.751C1.03756 11.5938 0.910149 11.2116 1.06732 10.8972Z" fill="currentColor" stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"></path></svg> */}
                 </li>
                 <li className="flex flex-wrap justify-end items-center gap-2">
                     {!smartAccount && !loading && !connected && (
