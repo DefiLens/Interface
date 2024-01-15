c3a67877d7a82407bbdb694f2c84773a58476ea9
sunnyRK <radadiyasunny970@gmail.com>
2024-01-15 12:23:13 +0530

    format portfolio container

--- modules/portfolio/PortfolioContainer.tsx
+++ modules/portfolio/PortfolioContainer.tsx
@@ -7,54 +7,51 @@ import { iPortfolio, usePortfolioStore } from "../../store/Portfolio";
 import { useAddress } from "@thirdweb-dev/react";
 
 const PortfolioContainer: React.FC<any> = () => {
-
     const { mutateAsync: fetchPortfolio } = usePortfolio();
     const address = useAddress();
 
-    const {
-        smartAccountAddress,
-    }: iGlobal = useGlobalStore((state) => state);
+    const { smartAccountAddress }: iGlobal = useGlobalStore((state) => state);
 
-    const {
-        userTokensData,
-        setUserTokensData,
-        isUsersTokenLoading,
-        isSCW,
-    }: iPortfolio = usePortfolioStore((state) => state);
+    const { userTokensData, setUserTokensData, isUsersTokenLoading, isSCW }: iPortfolio = usePortfolioStore(
+        (state) => state
+    );
 
     const handleFetchPorfolioData = () => {
-        const  fetch = async (address: string) => {
-            await fetchPortfolio({ address })
-         }
+        const fetch = async (address: string) => {
+            await fetchPortfolio({ address });
+        };
 
-         if (smartAccountAddress) {
-            setUserTokensData(null)
+        if (smartAccountAddress) {
+            setUserTokensData(null);
             fetch(isSCW ? smartAccountAddress : address);
-         }
-    }
+        }
+    };
 
-    
     useEffect(() => {
-        handleFetchPorfolioData()
+        handleFetchPorfolioData();
     }, [smartAccountAddress, isSCW]);
 
-    const separatedArray = (userTokensData || []).filter((token: any) => token.type === "defiToken")
-    .reduce((acc, curr) => {
-        const key: any = curr.protocol?.name;
-        if (!acc[key]) {
-          acc[key] = [curr];
-        } else {
-          acc[key].push(curr);
-        }
-        return acc;
-      }, {});
+    const separatedArray = (userTokensData || [])
+        .filter((token: any) => token.type === "defiToken")
+        .reduce((acc, curr) => {
+            const key: any = curr.protocol?.name;
+            if (!acc[key]) {
+                acc[key] = [curr];
+            } else {
+                acc[key].push(curr);
+            }
+            return acc;
+        }, {});
 
-      const filteredDefiTokens = Object.values(separatedArray);
+    const filteredDefiTokens = Object.values(separatedArray);
 
-      const TotalNetWorth = (userTokensData || []).reduce((acc, curr) => {
-          acc.netWorth = parseFloat(curr.amount)
-        return acc;
-      }, { netWorth: 0});
+    const TotalNetWorth = (userTokensData || []).reduce(
+        (acc, curr) => {
+            acc.netWorth = parseFloat(curr.amount);
+            return acc;
+        },
+        { netWorth: 0 }
+    );
 
     return (
         <Portfolio
@@ -65,6 +62,6 @@ const PortfolioContainer: React.FC<any> = () => {
             handleFetchPorfolioData={handleFetchPorfolioData}
             totalNetWorth={TotalNetWorth.netWorth}
         />
-    )
+    );
 };
 export default PortfolioContainer;
