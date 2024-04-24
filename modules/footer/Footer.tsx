e5900c2d7835437cbbcb20a0c8c507d256a274da
Sarthak Verma <sarthakvdev@gmail.com>
2024-04-24 17:31:45 +0530

    Card component + Footer height fix

--- modules/footer/Footer.tsx
+++ modules/footer/Footer.tsx
@@ -3,21 +3,23 @@ import { tFooter } from "./types";
 
 const Footer: React.FC<tFooter> = ({ SocialHandles }) => {
     return (
-        <div className="w-full flex justify-center items-center gap-1 p-2">
-            <div className="flex justify-center items-center gap-5">
+        <div className="w-full flex justify-center">
+            <div className="inline-flex justify-center gap-5 h-full items-center">
                 {SocialHandles.length > 0 &&
                     SocialHandles.map((item) => (
-                        <div key={item.key} className="bg-white h-7 w-7">
-                            <a href={item.href} target="_blank" rel="noreferrer">
-                                <Image
-                                    src={item.icon}
-                                    alt={item.key}
-                                    width={100}
-                                    height={100}
-                                    className="h-full w-full"
-                                />
-                            </a>
-                        </div>
+                        <a
+                            href={item.href}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="hover:-translate-y-1 hover:scale-110 transition-transform ease-in-out"
+                        >
+                            <Image
+                                src={item.icon}
+                                alt={item.key}
+                                width={28}
+                                height={28}
+                            />
+                        </a>
                     ))}
             </div>
         </div>
