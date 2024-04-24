010592ef63b5f2065d49befa693aa242cfaabc20
Sarthak Verma <sarthakvdev@gmail.com>
2024-04-24 17:32:03 +0530

    onboarding comp/page poc

--- modules/trade/Trade.tsx
+++ modules/trade/Trade.tsx
@@ -8,10 +8,13 @@ import { iTrading, useTradingStore } from "../../store/TradingStore";
 import TokenSelectionMenu from "../../components/Batching/TokenSelectionMenu";
 import BatchSelectionSection from "../../components/Batching/BatchSelectionSection";
 import BatchingListSection from "../../components/Batching/BatchingListSection";
-import clsx from "clsx";
 import { iGlobal, useGlobalStore } from "../../store/GlobalStore";
+import { cn } from "../../lib/utils";
 import { useAddress } from "@thirdweb-dev/react";
 import ModalWrapper from "../../components/Models/ModalWrapper";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/Card";
+import Link from "next/link";
+import { HiExclamationCircle, HiMiniExclamationCircle } from "react-icons/hi2";
 
 bg.config({ DECIMAL_PLACES: 10 });
 
@@ -57,23 +60,25 @@ const Trade: React.FC<tTrade> = ({
 
     const { scwBalance }: iGlobal = useGlobalStore((state) => state);
     const [noScwBalance, setNoScwBalance] = useState<boolean>(false);
+    const [isOnboardOpen, setIsOnboardOpen] = useState<boolean>(false);
 
     const address = useAddress();
 
     useEffect(() => {
         const balance = address ? (!isNaN(parseFloat(scwBalance)) ? parseFloat(scwBalance) ?? 0 : 0) : 1;
         setNoScwBalance(balance <= 0);
+        setIsOnboardOpen(balance <= 0);
     }, [scwBalance, useAddress]);
 
     return (
         <div className="w-full h-full flex flex-col justify-center items-center py-5">
             <div
-                className={clsx(
+                className={cn(
                     showBatchList ? "!w-full" : "!w-[50%]",
                     "h-full flex flex-col lg:flex-row justify-start lg:justify-center items-center lg:items-start gap-4"
                 )}
             >
-                <div className="w-full md:max-w-xl h-full flex flex-col justify-start items-center">
+                <div className="w-full md:max-w-xl h-full flex flex-col justify-center items-center">
                     {/* FROM_TOKEN SELECTION MENU */}
                     <TokenSelectionMenu
                         showMenu={showFromSelectionMenu}
@@ -110,13 +115,21 @@ const Trade: React.FC<tTrade> = ({
                     />
 
                     {/* Batching Component */}
-                    <BatchSelectionSection
-                        handleSwap={handleSwap}
-                        onChangeAmountIn={onChangeAmountIn}
-                        sendSingleBatchToList={sendSingleBatchToList}
-                        handleExecuteMethod={handleExecuteMethod}
-                        processRebalancing={processRebalancing}
-                    />
+                    <div className="relative w-full">
+                        <BatchSelectionSection
+                            handleSwap={handleSwap}
+                            onChangeAmountIn={onChangeAmountIn}
+                            sendSingleBatchToList={sendSingleBatchToList}
+                            handleExecuteMethod={handleExecuteMethod}
+                            processRebalancing={processRebalancing}
+                        />
+                        {noScwBalance && <button
+                            className="absolute -top-4 -right-4 z-20 rounded-full border bg-white hover:scale-105 transition-transform"
+                            onClick={() => setIsOnboardOpen(true)}
+                        >
+                            <HiMiniExclamationCircle className="text-red-500 text-3xl" />
+                        </button>}
+                    </div>
                 </div>
 
                 {/* Modal - Select Execution Method */}
@@ -124,28 +137,71 @@ const Trade: React.FC<tTrade> = ({
 
                 {/* Modal - While Executing Batches */}
                 {showExecuteBatchModel && <ExecuteBatch />}
-                {/* {address ? <p>Has SCW balance: {noScwBalance ? "false" : "true"}</p> : <p>Connect Wallet</p>} */}
+
+                {/* No SCW Balance Card */}
+                {/* <Card className="absolute right-10 top-20 max-w-md bg-white text-black-600 shadow-lg">
+                    <CardHeader>
+                        <CardTitle>Welcome to DefiLens!</CardTitle>
+                        <CardDescription>
+                            and to the world of Account Abstraction
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex flex-col gap-4">
+                            <p>
+                                🚨 You need balance on smart-account to be able
+                                to Batch tokens.
+                            </p>
+                            <Link
+                                href="/onboarding"
+                                className="rounded-md bg-black/80 hover:bg-black/100 text-white max-w-fit px-4 py-2 hover:shadow-md transition-all"
+                            >
+                                Migrate Tokens
+                            </Link>
+                        </div>
+                    </CardContent>
+                    <CardFooter>
+                        <p className="text-slate-400 text-sm font-medium w-full">
+                            Note: Please contact us on our{" "}
+                            <a
+                                className="underline underline-offset-2"
+                                href="https://google.com"
+                                target="_blank"
+                            >
+                                telegram community
+                            </a>{" "}
+                            if you need help.
+                        </p>
+                    </CardFooter>
+                </Card> */}
 
                 <ModalWrapper
-                    open={noScwBalance}
-                    onOpenChange={setNoScwBalance}
+                    open={isOnboardOpen}
+                    onOpenChange={setIsOnboardOpen}
                     title="Welcome to DefiLens!"
                     description="and to the world of Account Abstraction"
                     footer={
                         <p className="text-slate-400 text-sm font-medium w-full">
                             Note: Please contact us on our{" "}
-                            <a className="underline underline-offset-2" href="https://google.com" target="_blank">
+                            <Link
+                                className="underline underline-offset-2"
+                                href="https://x.com/DefiLensTech"
+                                target="_blank"
+                            >
                                 telegram community
-                            </a>{" "}
+                            </Link>{" "}
                             if you need help.
                         </p>
                     }
                 >
                     <div className="flex flex-col gap-4">
                         <p>You need balance on smart-account to be able to Batch tokens.</p>
-                        <button className="rounded-md bg-black/80 hover:bg-black/100 text-white max-w-fit px-4 py-2 hover:shadow-md transition-all">
+                        <Link
+                            href="/onboarding"
+                            className="rounded-md bg-black/80 hover:bg-black/100 text-white max-w-fit px-4 py-2 hover:shadow-md transition-all"
+                        >
                             Migrate Tokens
-                        </button>
+                        </Link>
                     </div>
                 </ModalWrapper>
 
