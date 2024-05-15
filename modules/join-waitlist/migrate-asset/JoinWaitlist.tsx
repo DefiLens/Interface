fc6fcc8eb0dd0573bf565702c2d97223e694b0f7
Sarthak Verma <sarthakvdev@gmail.com>
2024-05-15 22:05:30 +0530

    waitlist form loader & hierarchy

--- modules/join-waitlist/migrate-asset/JoinWaitlist.tsx
+++ modules/join-waitlist/migrate-asset/JoinWaitlist.tsx
@@ -5,12 +5,16 @@ import { CgSpinner } from "react-icons/cg";
 import { tButton } from "../../../components/Button/types";
 import Link from "next/link";
 import toast from "react-hot-toast";
+import { cn } from "../../../lib/utils";
 
-const Button = ({ handleClick, isLoading = false, innerText }: tButton) => (
+const Button = ({ handleClick, isLoading = false, innerText, customStyle }: tButton) => (
     <button
         type="button"
         onClick={handleClick}
-        className="bg-black text-white w-full flex justify-center items-center gap-2  py-3 px-5 rounded-lg text-base md:text-lg font-bold transition duration-300"
+        className={cn(
+            "w-full flex justify-center items-center gap-2 py-3 px-5 rounded-lg text-base md:text-lg font-bold transition duration-300",
+            customStyle
+        )}
     >
         {isLoading && <CgSpinner className="animate-spin h-7 w-7" />}
         {innerText}
@@ -22,6 +26,7 @@ const JoinWaitlist = () => {
     const [step, setStep] = useState(1); // stepper
     const [email, setEmail] = useState("");
     const [isOpen, setIsOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [response, setResponse] = useState(null);
 
     // checking if user already visited
@@ -45,6 +50,7 @@ const JoinWaitlist = () => {
 
     // Submitting the email: validation -> post req -> save in localStorage -> update stepper
     const handleSubmitEmail = async () => {
+        setIsLoading(true);
         try {
             if (!isValidEmail(email)) {
                 toast.error("Please enter a valid email address.");
@@ -55,8 +61,11 @@ const JoinWaitlist = () => {
 
             localStorage.setItem("visited", "true");
             localStorage.setItem("email", email);
+
+            setIsLoading(false);
             setStep(3);
         } catch (error) {
+            setIsLoading(false);
             console.error("Error submitting email:", error);
         }
     };
@@ -68,27 +77,27 @@ const JoinWaitlist = () => {
     };
 
     return (
-        <div className={`fixed z-30 inset-0 overflow-y-auto ${isOpen ? "block" : "hidden"}`}>
+        <div className={cn("fixed z-30 inset-0 overflow-y-auto", isOpen ? "block" : "hidden")}>
             <div className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm backdrop-filter"></div>
             <div className="relative flex items-center justify-center min-h-screen">
                 <div className="flex flex-col bg-white rounded-lg shadow-lg p-8 max-w-[35rem] w-full relative">
                     {step === 1 && (
                         <>
-                            <h2 className="text-3xl font-bold mb-2 text-gray-800 font-sans">Welcome to Defilens</h2>
-                            <p className="text-lg text-gray-600 mb-6">
+                            <h2 className="text-3xl font-bold mb-1 text-gray-800 font-sans">Welcome to Defilens</h2>
+                            <p className="text-base font-medium text-gray-500 mb-6">
                                 Join the waitlist to become an early user of Defilens
                             </p>
-                            <div className="flex flex-col gap-3 max-w-[30rem] w-full">
+                            <div className="flex flex-col gap-5 max-w-[30rem] w-full">
                                 <Button
                                     handleClick={handleJoinWaitlist}
                                     isLoading={false}
-                                    customStyle=""
+                                    customStyle="bg-zinc-900 hover:bg-zinc-800 text-white"
                                     innerText="Join waitlist"
                                 />
                                 <Button
                                     handleClick={trySimulation}
                                     isLoading={false}
-                                    customStyle=""
+                                    customStyle="bg-zinc-100 text-black"
                                     innerText="Try Simulation"
                                 />
                             </div>
@@ -106,9 +115,9 @@ const JoinWaitlist = () => {
                             />
                             <Button
                                 handleClick={handleSubmitEmail}
-                                isLoading={false}
-                                customStyle=""
-                                innerText="Submit"
+                                isLoading={isLoading}
+                                customStyle="bg-zinc-900 hover:bg-zinc-800 text-white"
+                                innerText={isLoading ? "Submitting..." : "Submit"}
                             />
                         </>
                     )}
@@ -118,12 +127,12 @@ const JoinWaitlist = () => {
                             <Button
                                 handleClick={trySimulation}
                                 isLoading={false}
-                                customStyle=""
+                                customStyle="bg-zinc-900 hover:bg-zinc-800 text-white"
                                 innerText="Try Simulation"
                             />
                         </>
                     )}
-                    <p className="text-slate-500 text-sm font-medium w-full mt-3 font-sans">
+                    <p className="text-slate-500 text-sm font-medium w-full mt-6 font-sans">
                         Note: Please contact us on our{" "}
                         <Link
                             className="text-slate-600 underline underline-offset-2"
